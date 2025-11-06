0f857865448c38cce5a476a34b3081c3099ebeb7
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-11-06 21:13:32 +0530

    I've added some styles to the homepage

--- code/client/src/pages/ui/Home.jsx
+++ code/client/src/pages/ui/Home.jsx
@@ -10,9 +10,21 @@ import { Button } from "../../components/index.components";
 function Home() {
   return (
     <>
-      <section className="w-full h-full flex justify-center items-center">
-        This is the home page
-      </section>
+      <main className="w-full h-full flex flex-col justify-center items-center bg-gray-200">
+        {/* The heading of the homepage */}
+        <section className="flex flex-col pt-2 border w-full justify-center items-center text-sm">
+          <h1 className="font-sans font-extrabold text-5xl flex flex-col justify-center items-center">
+            <span>Task & Habit</span>
+            <span>Tracker</span>
+          </h1>
+          <h3 className="text-[13px]">Simplify Your Routine</h3>
+        </section>
+
+        {/* The body of the homepage */}
+        <section className="border w-full flex flex-col items-center justify-center">
+          <div className="border w-36 bg-white">hey</div>
+        </section>
+      </main>
       <Link to={"/users/register"}>
         <Button content={"Register"} />
       </Link>
