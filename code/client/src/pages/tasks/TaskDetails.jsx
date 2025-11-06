4dcd8a23600fff0b88729620edc9d1477ad4738f
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-11-06 11:33:09 +0530

    I added comments to my taskDetails page and included the task in the UI

--- code/client/src/pages/tasks/TaskDetails.jsx
+++ code/client/src/pages/tasks/TaskDetails.jsx
@@ -1,21 +1,40 @@
+/* ---------------------------------------------------------------------------
+TaskDetails.jsx
+This page shows the details of a specific task
+------------------------------------------------------------------------------ */
+
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getTask } from "../../features/index.features";
 
 function TaskDetails() {
-  const { taskId } = useParams();
-
+  // ----------------------------------------------------------------------------------
+  // All the variables
+  // ----------------------------------------------------------------------------------
+  const { taskId } = useParams(); // getting the task id included in the URL
   const dispatch = useDispatch();
 
+  // ----------------------------------------------------------------------------------
+  // Get the details of the task as soon as the page mounts
+  // ----------------------------------------------------------------------------------
   useEffect(() => {
     dispatch(getTask(taskId));
   }, [taskId, dispatch]);
 
-  const task = useSelector((state) => state.tasks.specificTask);
-  console.log("SPECIFIC TASK: ", task);
+  const task = useSelector((state) => state.tasks.specificTask.message); // the task data
 
-  return <h1>Hey</h1>;
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-black text-3xl">{task.title}</h1>
+        <p>{task.description}</p>
+        <span>Category: {task.category}</span>
+        <span>Priority: {task.priority}</span>
+        <span>Completion: {String(task.isCompleted)}</span>
+      </div>
+    </>
+  );
 }
 
 export default TaskDetails;
