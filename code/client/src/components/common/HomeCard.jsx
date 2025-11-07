83be6e0cc254990659db60114de36e0979b93a44
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-11-07 18:00:29 +0530

    Made some subtle design changes to my homepage

--- code/client/src/components/common/HomeCard.jsx
+++ code/client/src/components/common/HomeCard.jsx
@@ -10,14 +10,15 @@ import Button from "./Button";
 function HomeCard({ icon, heading, desc, styles = "" }) {
   return (
     <div
-      className={`rounded-lg flex flex-col justify-center items-center bg-white shadow-xl px-2 w-36 h-44 gap-3  sm:w-48 sm:h-56 sm:gap-5 md:w-56 md:h-64 md:gap-7 lg:w-66 lg:h-74 xl:w-92 xl:h-88 xl:gap-8 hover:border hover:border-gray-300 ${styles}`}
+      className={`rounded-2xl flex flex-col justify-center items-center bg-white shadow-xl px-2 w-36 h-44 gap-3  sm:w-48 sm:h-56 sm:gap-5 md:w-56 md:h-64 md:gap-7 lg:w-66 lg:h-74 xl:w-92 xl:h-88 xl:gap-8 hover:shadow-[0_10px_25px_rgba(0,0,0,0.15)] hover:scale-[1.03] transition-all duration-300 ease-out ${styles}`}
     >
       <FontAwesomeIcon
         icon={icon}
         style={{ color: "oklch(37.9% 0.146 265.522)" }}
+        className="sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl]"
       />
       <div className="flex flex-col justify-center items-center xl:gap-3">
-        <h5 className="text-center text-lg sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
+        <h5 className="text-center text-lg sm:text-2xl md:text-3xl lg:text-4xl">
           {heading}
         </h5>
         <span className="font-extralight text-center text-gray-700 text-sm sm:text-md lg:text-lg">
--- code/client/src/pages/ui/Home.jsx
+++ code/client/src/pages/ui/Home.jsx
@@ -18,20 +18,20 @@ function Home() {
   return (
     <>
       {/* The main section */}
-      <main className="w-full h-full flex flex-col justify-center items-center bg-gray-200 py-5">
+      <main className="w-full min-h-screen flex flex-col justify-center items-center bg-linear-to-b from-gray-100 to-gray-200 py-10 px-2 transition-all duration-300">
         {/* The heading of the homepage */}
-        <section className="flex flex-col w-full justify-center items-center text-sm gap-1 px-1">
-          <h1 className="font-sans font-extrabold flex flex-col justify-center items-center text-5xl sm:text-7xl md:text-8xl xl:text-10xl">
+        <section className="flex flex-col w-full justify-center items-center text-center gap-2 px-2 mb-8">
+          <h1 className="font-sans font-extrabold flex flex-col justify-center items-center text-5xl sm:text-7xl md:text-8xl xl:text-9xl text-gray-900 leading-tight drop-shadow-sm">
             <span>Task & Habit</span>
             <span>Tracker</span>
           </h1>
-          <h3 className="font-sans text-gray-900 font-semibold text-[13px] sm:text-lg md:text-xl">
+          <h3 className="font-sans text-gray-700 font-semibold text-[13px] sm:text-lg md:text-xl tracking-wide">
             Simplify Your Routine
           </h3>
         </section>
 
         {/* The body of the homepage */}
-        <section className="w-full flex flex-wrap items-center justify-center gap-5 font-semibold py-4 px-1 sm:px-3 xl:gap-10">
+        <section className="w-full flex flex-wrap items-center justify-center gap-6 sm:gap-8 md:gap-10 xl:gap-12 font-semibold py-6 px-3 transition-all duration-300">
           {/* THE CARD COMPONENTS */}
 
           {/* The first card (Daily Tasks) */}
@@ -80,27 +80,33 @@ function Home() {
         </section>
 
         {/* The registeration section */}
-        <section className="flex justify-center items-center w-full gap-5 p-2 xl:pt-6">
+        <section className="flex flex-wrap justify-center items-center w-full gap-6 sm:gap-8 md:gap-10 p-4 xl:pt-10">
           <Link to={"/users/register"}>
             <Button
               content={"Register"}
-              styles="p-5 w-40 text-lg sm:w-56 sm:text-2xl sm:p-6 md:w-64 md:p-7 md:text-3xl lg:w-72 md:p-8 md:text-4xl duration-150"
+              styles="p-5 w-40 text-lg sm:w-56 sm:text-2xl sm:p-6 md:w-64 md:p-7 md:text-3xl lg:w-72 lg:p-8 lg:text-4xl bg-blue-900 hover:bg-blue-800 text-white rounded-xl shadow-md hover:shadow-lg transition-all duration-300"
             />
           </Link>
           <Link to={"/users/login"}>
             <Button
               content={"Log in"}
-              styles="p-5 w-40 text-lg sm:w-56 sm:text-2xl sm:p-6 md:w-64 md:p-7 md:text-3xl lg:w-72 md:p-8 md:text-4xl"
+              styles="p-5 w-40 text-lg sm:w-56 sm:text-2xl sm:p-6 md:w-64 md:p-7 md:text-3xl lg:w-72 lg:p-8 lg:text-4xl bg-gray-700 hover:bg-gray-800 text-white rounded-xl shadow-md hover:shadow-lg transition-all duration-300"
             />
           </Link>
         </section>
       </main>
 
       {/* The footer slogan */}
-      <footer className="flex justify-center items-center gap-4 bg-gray-200 w-full pb-5 text-gray-700 sm:text-lg md:text-xl lg:text-2xl">
-        <span>Track</span>
-        <span>Achieve</span>
-        <span>Repeat</span>
+      <footer className="flex justify-center items-center gap-6 bg-gray-200 w-full py-6 text-gray-700 sm:text-lg md:text-xl lg:text-2xl tracking-wide uppercase font-medium border-t border-gray-300">
+        <span className="hover:text-gray-900 transition-colors duration-300">
+          Track
+        </span>
+        <span className="hover:text-gray-900 transition-colors duration-300">
+          Achieve
+        </span>
+        <span className="hover:text-gray-900 transition-colors duration-300">
+          Repeat
+        </span>
       </footer>
     </>
   );
