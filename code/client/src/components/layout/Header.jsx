5ee36a33f6dd1a59d14e59e1b91117c2425f5d60
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-11-08 23:24:04 +0530

    added comments to the header

--- code/client/src/components/layout/Header.jsx
+++ code/client/src/components/layout/Header.jsx
@@ -11,14 +11,18 @@ import { faUserTie } from "@fortawesome/free-solid-svg-icons";
 import useConditionalRendering from "../../hooks/useConditionalRendering";
 
 function Header() {
-  const { user } = useConditionalRendering("users");
-  const [toLogOut, setToLogOut] = useState(false);
+  const { user } = useConditionalRendering("users"); // for displaying the profile pic
+  const [toLogOut, setToLogOut] = useState(false); // for displaying the logout button
 
   return (
+    // The header section
     <header className="w-full h-18 border-b border-blue-200 fixed z-50 shadow-xl bg-white py-2">
+      {/* The nav bar */}
       <nav className="w-full h-full flex flex-col px-5 items-center font-semibold text-lg">
         <div className="w-full flex items-center justify-end">
+          {/* The profile section */}
           <div className="flex flex-col gap-1 justify-center items-center">
+            {/* By clicking on the profile, the user can see the logout button */}
             <span
               className="border border-blue-900 rounded-full overflow-hidden w-6 h-6 flex justify-center items-center sm:w-16 sm:h-16 lg:w-26 lg:h-26 bg-blue-50"
               onClick={() => {
@@ -26,6 +30,7 @@ function Header() {
               }}
               title="logout"
             >
+              {/* If a profile is uploaded, display it or else a dummy image */}
               {user?.message?.profilePic === "" ? (
                 <FontAwesomeIcon
                   icon={faUserTie}
@@ -40,13 +45,15 @@ function Header() {
                 />
               )}
             </span>
-            <span
+
+            {/* The logout button */}
+            <button
               className={`text-[9px] font-semibold border w-16 text-center rounded-sm bg-gray-200 fixed top-9 ${
                 toLogOut ? "visible" : "hidden"
               } cursor-pointer`}
             >
               Log out
-            </span>
+            </button>
           </div>
         </div>
         <div className="w-full flex justify-around">
