ecc8b7f9607684cfb93c821475f268f89936fc40
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-11-10 10:56:01 +0530

    I updated the mobile design of my profile page

--- code/client/src/pages/users/Profile.jsx
+++ code/client/src/pages/users/Profile.jsx
@@ -35,11 +35,13 @@ function Profile() {
       <MainSection styles="pt-20 pb-3">
         {/* Only if the user is authenticated */}
         {user ? (
-          <section className="w-full h-full flex flex-col items-center justify-center gap-2">
-            <h1 className="text-4xl font-bold">My Profile</h1>
+          <section className="w-full h-full flex flex-col items-center justify-center gap-5">
+            <h1 className="text-4xl font-bold text-blue-950 tracking-wide mb-2">
+              My Profile
+            </h1>
 
             {/* The profile image */}
-            <div className="bg-white p-2 flex justify-center items-center w-full h-48 rounded-3xl shadow-md">
+            <div className="bg-white relative p-3 flex justify-center items-center w-full h-52 rounded-3xl shadow-lg hover:shadow-xl transition-all duration-300">
               <img
                 src={user?.profilePic}
                 className="w-36 h-36 rounded-full"
@@ -49,29 +51,34 @@ function Profile() {
 
               <Link
                 to={"/users/me/appearance"}
-                className="flex justify-center items-center bg-blue-900 w-7 h-7 rounded-full border border-black relative right-9 top-14 cursor-pointer"
+                className="flex justify-center items-center bg-blue-900 w-8 h-8 rounded-full border border-gray-800 shadow-md absolute right-[30%] top-[67%] hover:bg-blue-950 transition-all duration-200"
                 title="update pic"
               >
                 <FontAwesomeIcon icon={faPen} className="text-white text-sm" />
               </Link>
             </div>
-            <div className="bg-white p-2 flex flex-col justify-start items-center gap-3 w-full h-48 rounded-3xl shadow-md">
+
+            {/* The details section */}
+            <div className="bg-white p-5 flex flex-col justify-between items-center gap-5 w-full h-auto rounded-3xl shadow-lg hover:shadow-xl transition-all duration-300">
               {/* The details */}
-              <div className="flex flex-col items-start justify-between">
-                <h2 className="text-2xl font-semibold">
+              <div className="flex flex-col items-start justify-center text-center gap-1">
+                <h2 className="text-2xl font-semibold text-gray-900">
                   {`${user?.fullName?.firstName} ${user?.fullName?.lastName}`}
                 </h2>
-                <h5 className="text-xs text-gray-800">{user?.email}</h5>
-                <h5 className="text-[10px] text-gray-800">@{user?.username}</h5>
+                <h5 className="text-sm text-gray-600">{user?.email}</h5>
+                <h5 className="text-xs text-gray-500 italic">
+                  @{user?.username}
+                </h5>
               </div>
+
               {/*  ----------------------------------------------------------------------------------
               The links to update the profile
-            ---------------------------------------------------------------------------------- */}
-              <div className="flex flex-col gap-2">
+              ---------------------------------------------------------------------------------- */}
+              <div className="flex flex-col gap-3">
                 <Link to={"/users/me/details"} className="cursor-pointer">
                   <Button
                     content={"Update Details"}
-                    styles="rounded-2xl text-sm p-5"
+                    styles="rounded-2xl text-sm p-5 font-semibold tracking-wide shadow-md hover:shadow-lg transition-all duration-300"
                     bgColor="bg-blue-900 hover:bg-blue-950"
                     width="w-55"
                   />
@@ -79,7 +86,7 @@ function Profile() {
                 <Link to={"/users/me/password"} className="cursor-pointer">
                   <Button
                     content={"Change Password"}
-                    styles="rounded-2xl text-sm p-5"
+                    styles="rounded-2xl text-sm p-5 font-semibold tracking-wide shadow-md hover:shadow-lg transition-all duration-300"
                     bgColor="bg-blue-950 hover:bg-blue-900"
                     width="w-55"
                   />
