3b08ae6b0a46b251b23e33464f733a003edfb147
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-11-10 11:29:31 +0530

    updated my header component and styled my profile page for larger screens

--- code/client/src/pages/users/Profile.jsx
+++ code/client/src/pages/users/Profile.jsx
@@ -3,7 +3,7 @@ Profile.jsx
 This is the profile section of the user. The user can delete their profile here. 
 ------------------------------------------------------------------------------ */
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { getUser } from "../../features/index.features";
 import { useDispatch, useSelector } from "react-redux";
 import { Button, MainSection } from "../../components/index.components";
@@ -36,70 +36,75 @@ function Profile() {
         {/* Only if the user is authenticated */}
         {user ? (
           <section className="w-full h-full flex flex-col items-center justify-center gap-5">
-            <h1 className="text-4xl font-bold text-blue-950 tracking-wide mb-2">
+            <h1 className="text-4xl font-bold text-blue-950 tracking-wide mb-2 sm:text-5xl">
               My Profile
             </h1>
-
-            {/* The profile image */}
-            <div className="bg-white relative p-3 flex justify-center items-center w-full h-52 rounded-3xl shadow-lg hover:shadow-xl transition-all duration-300">
-              <img
-                src={user?.profilePic}
-                className="w-36 h-36 rounded-full"
-                title="profile"
-                alt="Profile Picture"
-              />
-              <div className="w-34 h-36 absolute flex justify-end items-end">
-                <Link
-                  to={"/users/me/appearance"}
-                  className="flex justify-center items-center bg-blue-900 w-8 h-8 rounded-full border border-gray-800 shadow-md absolute hover:bg-blue-950 transition-all duration-200"
-                  title="update pic"
-                >
-                  <FontAwesomeIcon
-                    icon={faPen}
-                    className="text-white text-sm"
-                  />
-                </Link>
-              </div>
-            </div>
-
-            {/* The details section */}
-            <div className="bg-white p-5 flex flex-col justify-between items-center gap-5 w-full h-auto rounded-3xl shadow-lg hover:shadow-xl transition-all duration-300">
-              {/* The details */}
-              <div className="flex flex-col items-start justify-center text-center gap-1">
-                <h2 className="text-2xl font-semibold text-gray-900">
-                  {`${user?.fullName?.firstName} ${user?.fullName?.lastName}`}
-                </h2>
-                <h5 className="text-sm text-gray-600">{user?.email}</h5>
-                <h5 className="text-xs text-gray-500 italic">
-                  @{user?.username}
-                </h5>
+            <div className="w-full h-full flex flex-col items-center justify-center gap-5 sm:flex-row">
+              {/* The profile image */}
+              <div className="bg-white relative p-3 flex justify-center items-center w-full h-52 rounded-3xl shadow-lg hover:shadow-xl transition-all duration-300 sm:h-66">
+                <img
+                  src={user?.profilePic}
+                  className="w-36 h-36 rounded-full"
+                  title="profile"
+                  alt="Profile Picture"
+                />
+                <div className="w-34 h-36 absolute flex justify-end items-end">
+                  <Link
+                    to={"/users/me/appearance"}
+                    className="flex justify-center items-center bg-blue-900 w-8 h-8 rounded-full border border-gray-800 shadow-md absolute hover:bg-blue-950 transition-all duration-200"
+                    title="update pic"
+                  >
+                    <FontAwesomeIcon
+                      icon={faPen}
+                      className="text-white text-sm"
+                    />
+                  </Link>
+                </div>
               </div>
 
-              {/*  ----------------------------------------------------------------------------------
+              {/* The details section */}
+              <div className="bg-white p-5 flex flex-col justify-center items-center gap-5 w-full h-auto rounded-3xl shadow-lg hover:shadow-xl transition-all duration-300 sm:h-66">
+                {/* The details */}
+                <div className="flex flex-col items-start justify-center text-center gap-1">
+                  <h2 className="text-2xl font-semibold text-gray-900 sm:text-3xl">
+                    {`${user?.fullName?.firstName} ${user?.fullName?.lastName}`}
+                  </h2>
+                  <h5 className="text-sm text-gray-600 sm:text-md">
+                    {user?.email}
+                  </h5>
+                  <h5 className="text-xs text-gray-500 italic sm:text-sm">
+                    @{user?.username}
+                  </h5>
+                </div>
+
+                {/*  ----------------------------------------------------------------------------------
               The links to update the profile
               ---------------------------------------------------------------------------------- */}
-              <div className="flex flex-col gap-3">
-                <Link to={"/users/me/details"} className="cursor-pointer">
-                  <Button
-                    content={"Update Details"}
-                    styles="rounded-2xl text-sm p-5 font-semibold tracking-wide shadow-md hover:shadow-lg transition-all duration-300"
-                    bgColor="bg-blue-900 hover:bg-blue-950"
-                    width="w-55"
-                  />
-                </Link>
-                <Link to={"/users/me/password"} className="cursor-pointer">
-                  <Button
-                    content={"Change Password"}
-                    styles="rounded-2xl text-sm p-5 font-semibold tracking-wide shadow-md hover:shadow-lg transition-all duration-300"
-                    bgColor="bg-blue-950 hover:bg-blue-900"
-                    width="w-55"
-                  />
-                </Link>
+                <div className="flex flex-col gap-3">
+                  <Link to={"/users/me/details"} className="cursor-pointer">
+                    <Button
+                      content={"Update Details"}
+                      styles="rounded-2xl text-sm p-5 font-semibold tracking-wide shadow-md hover:shadow-lg transition-all duration-300 sm:p-6 sm:text-lg"
+                      bgColor="bg-blue-900 hover:bg-blue-950"
+                      width="w-55 sm:w-66"
+                    />
+                  </Link>
+                  <Link to={"/users/me/password"} className="cursor-pointer">
+                    <Button
+                      content={"Change Password"}
+                      styles="rounded-2xl text-sm p-5 font-semibold tracking-wide shadow-md hover:shadow-lg transition-all duration-300 sm:p-6 sm:text-lg"
+                      bgColor="bg-blue-950 hover:bg-blue-900"
+                      width="w-55 sm:w-66"
+                    />
+                  </Link>
+                </div>
               </div>
             </div>
           </section>
         ) : (
-          <span>You're unauthorized!</span>
+          <span className="text-xl italic text-center sm:ml-15">
+            You're unauthorized!
+          </span>
         )}
       </MainSection>
     );
