02e9b04f2e51a7f7d24d37c6b69cd3f06f3d21a6
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-11-10 12:11:39 +0530

    completed the overall design of my profile page

--- code/client/src/pages/users/Profile.jsx
+++ code/client/src/pages/users/Profile.jsx
@@ -41,7 +41,7 @@ function Profile() {
             </h1>
             <div className="w-full h-full flex flex-col items-center justify-center gap-5 sm:flex-row">
               {/* The profile image */}
-              <div className="bg-white relative p-3 flex justify-center items-center w-full h-52 rounded-3xl shadow-lg hover:shadow-xl transition-all duration-300 sm:h-66">
+              <div className="bg-white relative p-3 flex justify-center items-center w-72 h-52 rounded-3xl shadow-lg hover:shadow-xl transition-all duration-300 sm:h-66 lg:h-70 sm:w-80 lg:w-95">
                 <img
                   src={user?.profilePic}
                   className="w-36 h-36 rounded-full"
@@ -63,16 +63,16 @@ function Profile() {
               </div>
 
               {/* The details section */}
-              <div className="bg-white p-5 flex flex-col justify-center items-center gap-5 w-full h-auto rounded-3xl shadow-lg hover:shadow-xl transition-all duration-300 sm:h-66">
+              <div className="bg-white p-5 flex flex-col justify-center items-center gap-5 w-72 h-auto rounded-3xl shadow-lg hover:shadow-xl transition-all duration-300 sm:h-66 sm:w-80 lg:h-70 lg:w-95">
                 {/* The details */}
                 <div className="flex flex-col items-start justify-center text-center gap-1">
-                  <h2 className="text-2xl font-semibold text-gray-900 sm:text-3xl">
+                  <h2 className="text-2xl font-semibold text-gray-900 sm:text-3xl lg:text-4xl">
                     {`${user?.fullName?.firstName} ${user?.fullName?.lastName}`}
                   </h2>
-                  <h5 className="text-sm text-gray-600 sm:text-md">
+                  <h5 className="text-sm text-gray-600 sm:text-md lg:text-lg">
                     {user?.email}
                   </h5>
-                  <h5 className="text-xs text-gray-500 italic sm:text-sm">
+                  <h5 className="text-xs text-gray-500 italic sm:text-sm lg:text-md">
                     @{user?.username}
                   </h5>
                 </div>
@@ -84,17 +84,17 @@ function Profile() {
                   <Link to={"/users/me/details"} className="cursor-pointer">
                     <Button
                       content={"Update Details"}
-                      styles="rounded-2xl text-sm p-5 font-semibold tracking-wide shadow-md hover:shadow-lg transition-all duration-300 sm:p-6 sm:text-lg"
+                      styles="rounded-2xl text-sm p-5 font-semibold tracking-wide shadow-md hover:shadow-lg transition-all duration-300 sm:p-6 sm:text-lg lg:text-xl lg:p-7"
                       bgColor="bg-blue-900 hover:bg-blue-950"
-                      width="w-55 sm:w-66"
+                      width="w-55 sm:w-66 lg:w-70"
                     />
                   </Link>
                   <Link to={"/users/me/password"} className="cursor-pointer">
                     <Button
                       content={"Change Password"}
-                      styles="rounded-2xl text-sm p-5 font-semibold tracking-wide shadow-md hover:shadow-lg transition-all duration-300 sm:p-6 sm:text-lg"
+                      styles="rounded-2xl text-sm p-5 font-semibold tracking-wide shadow-md hover:shadow-lg transition-all duration-300 sm:p-6 sm:text-lg lg:text-xl lg:p-7"
                       bgColor="bg-blue-950 hover:bg-blue-900"
-                      width="w-55 sm:w-66"
+                      width="w-55 sm:w-66 lg:w-70"
                     />
                   </Link>
                 </div>
