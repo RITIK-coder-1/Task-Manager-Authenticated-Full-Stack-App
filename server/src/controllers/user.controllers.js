4f1eccfb2d9a7e7b1950de8bec8bb2dd891cbff3
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-01-05 19:41:42 +0530

    Updated the comment for the access token controller

--- server/src/controllers/user.controllers.js
+++ server/src/controllers/user.controllers.js
@@ -243,7 +243,7 @@ const logoutFunction = async (req, res) => {
 
 const newAccessTokenFunction = async (req, res) => {
   try {
-    // Getting our refresh token from the cookies or request body alternatively
+    // Getting our refresh token from the cookies alternatively
     const incomingRefreshToken = req.cookies?.refreshToken;
 
     if (!incomingRefreshToken) {
